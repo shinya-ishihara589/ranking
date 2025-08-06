2e24741fb3518782215ebae1677b45181f3e0c5b
ishihara_shinya <s.ishihara5899@gmail.com>
2025-08-07 07:51:08 +0900

    プロフィール画面の変更

--- backend/app/View/Components/Modals/EditProfileModal.php
+++ backend/app/View/Components/Modals/EditProfileModal.php
@@ -18,11 +18,11 @@ final class EditProfileModal extends BaseModal
         $this->mainCategory = ['title' => 'プロフィール更新', 'id' => 'edit-profile-modal', 'form' => 'edit-profile-form'];
 
         // サブカテゴリーを単体で設定値を取得する
-        $tmpUserId = $this->setSubCategorie('text', 'edit_profile_name', 'edit_profile_name_error', '項目名');
-        $tmpUserId = $this->setSubCategorie('text', 'edit_profile_self_introduction', 'edit_profile_self_introduction_error', '項目名');
+        $userName = $this->setSubCategorie('text', 'edit_profile_name', 'edit_profile_name_error', '名前');
+        // $tmpUserId = $this->setSubCategorie('text', 'edit_profile_self_introduction', 'edit_profile_self_introduction_error', '項目名');
 
         // サブカテゴリ―を結合する
-        $this->subCategories = [$tmpUserId];
+        $this->subCategories = [$userName];
 
         // 閉じるボタンを取得する
         $this->closeButton = ['name' => '閉じる'];
